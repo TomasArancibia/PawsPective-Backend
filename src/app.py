d1f9475f6dd896275da8f51a74a1acb4c78e155c
tomas arancibia domper <tomasarancibia@uc.cl>
2024-05-23 13:10:31 -0400

    Fetch CreatePost

--- src/app.py
+++ src/app.py
@@ -2,19 +2,33 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
+import datetime
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Location, Media, Like, Comment, Follower, Feed 
+from models import db, User, Location, Media, Like, Comment, Follower, Feed, Post
+from werkzeug.utils import secure_filename
+import cloudinary
+import cloudinary.uploader
 #from models import Person
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ModelesDB.db"
+app.config['UPLOAD_FOLDER'] = "instance/photos"
 db.init_app(app)
+CORS(app)
 migrate = Migrate(app, db)
+
+          
+cloudinary.config( 
+  cloud_name = "dp1nq9zvq", 
+  api_key = "678483169239916", 
+  api_secret = "1_8mRP83pNvQNQVqpu8j5hQyv6Q" 
+)
 
 @app.route('/', methods=['GET'])
 def sitemap():
@@ -61,6 +75,37 @@ def update_or_delete_user(user_id):
         db.session.delete(user)
         db.session.commit()
         return jsonify({'message': 'User deleted successfully'}), 200
+
+@app.route('/feed/new_post', methods=['POST'])
+def new_post():
+    if 'post_data' not in request.form or 'source_url' not in request.files:
+        return jsonify({'error': 'Invalid request'}), 400
+    data = request.form['post_data']
+    file = request.files['source_url']
+    try:
+        data = json.loads(data)
+    except ValueError:
+        return jsonify({'error': 'Invalid JSON'}), 400
+    
+  
+    new_post = Post()
+    new_post.description = data["description"]
+    new_post.user_id = 1
+    new_post.likecount = 0
+    new_post.location = 0
+    new_post.feed_id = 1
+    new_post.date = datetime.datetime.now()
+
+    
+    try:
+        upload_result = cloudinary.uploader.upload(file)
+        new_post.source_url = upload_result['secure_url']
+    except Exception as e:
+        return jsonify({'error': 'Error uploading to Cloudinary', 'details': str(e)}), 500
+
+    db.session.add(new_post)
+    db.session.commit()
+    return jsonify({'message': 'Post created successfully', 'image_url': new_post.source_url}), 201
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
